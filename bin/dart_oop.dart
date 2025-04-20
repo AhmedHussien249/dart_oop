f7a8da2bcb9ca7399b070ce10128e1a8567db15c
ahmedhussien12 <ahmed.hussien950.ah@gmail.com>
2025-04-20 18:32:59 +0200

    ubdated

--- bin/dart_oop.dart
+++ bin/dart_oop.dart
@@ -1,6 +1,75 @@
+import 'dart:io';
+
 import 'oop.dart';
 void main() {
-  Human ahmed = Human(age: 27,height: 180,weight: 80,hairColor: "black");
-  ahmed.setNumberOfArms(1);
-  print(ahmed.getNumberOfArms());
+  Human ahmed = Human( 27, 180, 80, "black","A+");
+  ahmed.numberOfArms = 10;
+  print("***** ahmed object");
+  print(ahmed.numberOfArms);
+  print(ahmed.bloodType);
+
+  Lion scar = Lion(100, "brown");
+  print("*****lion object");
+  print(scar.weight);
+  print(scar.skinColor);
+  scar.eat();
+
+  Dog dog = Dog( 100,  "white", bloodType: "A",);
+  print("*****dog object");
+  print(dog.bloodType);
+  print(dog.weight);
+  print(dog.skinColor);
+  dog.numberOfLimbs=6;
+  print(dog.numberOfLimbs);
+  dog.eat();
+  dog.sleep();
+  dog.walk();
+  dog.bark();
+  dog.play();
+
+  Snake snake = Snake(100, "brown");
+  snake.crawl();
+
+  // polymorphism : mot3dd el ashkal
+  List<Animal> zoo = [scar, dog];
+  // fa kda class el animal tab2 nazryt el polymorphism
+
+
+  Circle c1 = Circle(radius: 5,x: 10,y: 10);
+  print("********c1");
+  c1.drawCircle();
+  Circle c2 = Circle.origin(5);
+  print("*******c2");
+  c2.drawCircle();
+
+  // anonymous object
+  List<Animal> animals =[Dog(200,"white"),Lion(200,"brown"),Snake(200,"brown" )];
+  animals[0].sleep();
+
+
+  Gender gender;
+  print("enter your gender");
+  gender = stdin.readLineSync() == "male" ? Gender.male : Gender.female;
+
+  switch (gender) {
+    case Gender.male:
+      print("your gender male");
+      break;
+    case Gender.female:
+      print("your gender female");
+      break;
+    case null:
+      // TODO: Handle this case.
+      print("enter your gender");
+    case Gender.other:
+      // TODO: Handle this case.
+      print("i don't know");
+  }
+
+
 }
+
+
+// enum : bst5dmha lma akon 3ayz a3ml mgmo3a mn el kym m7doda y3ni a2dr a3dhm
+
+enum Gender { male, female, other }
--- bin/oop.dart
+++ bin/oop.dart
@@ -1,5 +1,11 @@
 // 1- classes , object , constructors and methods
 // 2 - encapsulation
+// 3- inheritance
+// 4- polymorphism
+// 5- abstract class
+// 6- implements
+// 7- named constructor
+// 8- mixins
 
 class Human {
   // attributes
@@ -8,28 +14,48 @@ class Human {
   double? weight;
   double? height;
   String? hairColor;
+  String? bloodType;
 
   // constructor
-  Human({double? age, double? height, double? weight, String? hairColor}) {
-    this.age = age;
-    this.height = height;
-    this.weight = weight;
-    this.hairColor = hairColor;
-  }
 
-  // encapsulation methods
-  void setNumberOfArms(int numberOfArms) {
+  // d el tre2a el badena
+  // Human({double? age, double? height, double? weight, String? hairColor}) {
+  //   this.age = age;
+  //   this.height = height;
+  //   this.weight = weight;
+  //   this.hairColor = hairColor;
+  // }
+
+  // el tre2a el a7sn 3shan tdy baynat le attributes
+  // w tdaf f el attributes ely f el class
+  Human(this.age, this.height, this.weight, this.hairColor, [this.bloodType]);
+
+  // encapsulation methods el badena
+  // void setNumberOfArms(int numberOfArms) {
+  //   if (numberOfArms <= 2 && numberOfArms >= 0) {
+  //     this._numberOfArms = numberOfArms;
+  //   } else {
+  //     print("impossible");
+  //   }
+  // }
+  // int getNumberOfArms() {
+  //   return this._numberOfArms;
+  // }
+
+  // arrow function
+  // int getNumberOfArms()=>this._numberOfArms;
+
+  // setter
+  set numberOfArms(int numberOfArms) {
     if (numberOfArms <= 2 && numberOfArms >= 0) {
       this._numberOfArms = numberOfArms;
-    } else {
-      print("impossible");
     }
   }
 
-  int getNumberOfArms() {
-    return _numberOfArms;
-  }
+  // getter
+  int get numberOfArms => this._numberOfArms;
 }
+
 // class
 // leh b3ml class 3shan a3rf a3ml fsela w adeha 5awas w semat
 // k2ny 3ml mot8yr kda
@@ -62,3 +88,149 @@ class Human {
 // ezay b3mlo : 3n tre2 edaft (_) abl el attribute
 // w sa3tha b2a a2dr a3ml method f el class w at7km zy ma ana 3ayz
 // bs lazm a3ml 2 method w7da tdy kema w w7da trg3ly el kema
+// w el 2 method dol b2a el badelhm el setter w el getter
+// bykon shaklhm a7sn f el ktaba w b2dr at3aml m3ahm k2nhm attribute
+
+// inheritance:
+abstract class Animal {
+  int? weight;
+
+  String? skinColor;
+  int numberOfLimbs = 4;
+
+  Animal(this.weight, this.skinColor);
+
+  eat();
+
+  void sleep() {}
+}
+
+mixin AnimalsCanWalkMixin {
+  void walk() {
+    print("walking");
+  }
+}
+
+mixin ReptilesMixin {
+  void crawl() {
+    print("crawling");
+  }
+}
+
+mixin PetsMixin {
+  void play() {
+    print("playing");
+  }
+}
+
+class Lion extends Animal with AnimalsCanWalkMixin {
+  Lion(super.weight, super.skinColor);
+
+  @override
+  void eat() {
+    print("eating meat");
+  }
+
+  void roar() {
+    print("roaring");
+  }
+}
+
+// implements
+class Dog with AnimalsCanWalkMixin, PetsMixin implements Animal {
+  String? bloodType;
+
+  @override
+  eat() {
+    print("eating");
+  }
+
+  void bark() {
+    print("barking");
+  }
+
+  @override
+  int numberOfLimbs = 4;
+
+  @override
+  String? skinColor;
+
+  @override
+  int? weight;
+
+  Dog(this.weight, this.skinColor, {this.bloodType});
+
+  @override
+  void sleep() {
+    print("sleeping");
+  }
+}
+
+class Snake extends Animal with ReptilesMixin {
+  Snake(super.weight, super.skinColor);
+
+  @override
+  eat() {
+    print("eat mouses");
+  }
+}
+
+class Circle {
+  double radius;
+  late double y;
+  late double x;
+
+  Circle({required this.radius, required this.y, required this.x});
+
+  drawCircle() {
+    print("draw circle with radius :$radius at x:$x y:$y");
+  }
+
+  // named constructor
+  Circle.origin(this.radius) {
+    this.y = 0;
+    this.x = 0;
+  }
+}
+
+// super constructor
+// 5ly balk a hwa el suber
+// el super d bst5dmo 3shan ashawr 3la el constructor
+// el ab bdl ma ady le el constructor el bywrtho this
+// 3shan ashawr 3la el attributes bst5dm super
+
+// 5ly balk b2a
+// lw ktbt gwa el parameter data leha ashkal
+// 1- mn8er akwas 5als d b2a required 3ady
+// 2- {} b el akwas d b2a optional named
+// 3- {} b el akwas d w fe ablo required b2a required named
+// 4- [] w d optional mn8er named
+
+// override methods
+// hya nfs el method el f el class el ab
+// bs mt3dl 3leha w m3molha e3adt ktaba
+
+// abstract class :
+// d class bs feha method el method d mfehash code
+// bt5leny mogbr lma a3ml class tany bywrs el abstract class
+// eny a3ml override le el method d w adehla el 5was el ana 3yzha
+// 5ly balk el abstract class mynf3sh a5od mno object hwa byst3ml f el wrasa bs
+// ynf3 ast5dmo ka type bs
+
+// implements :
+//d class bywrs class tany w lazm y3ml override le kol 7aga feh
+
+// named constructor :
+// bst5dmo lw 3ayz a3ml constructor b attributes tanya
+
+// mixins :
+// d b2a ya m3lm shabh el class b2a el fr2 en ana mmkn adefo ytwrs m3 class
+// wars class tany w a2dr a3ml kza mixin w w a5ly el class yb2a wars class tany
+// w wars aktr mn mixin w lw fe sefat moshtrka f el kza mixin el hwa warshm
+// bya5od el sefa el mwgoda f a5r mixin
+
+
+// 5lek b2a 3arf en ay class enta bt3mlo bykon wars el OBJECT CLASS
+
+// 5lek fakr en fe 7aga mohma 2yma 3leha flutter w hya el ANONYMOUS OBJECT
+// bt5leny a3ml el object w a3ml access 3leh mn8er ma adelo esm
